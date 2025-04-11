c958ac33d472e6be31776da641c44e49fe1a665f
archisman <archiarchisman@gmail.com>
2025-04-12 01:59:07 +0530

    feat: db connection

---
+++ src/main/java/com/Surveying_Bot/SurveyingBotApplication.java
@@ -0,0 +1,13 @@
+package com.Surveying_Bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyingBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Surveying_Bot/SurveyingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Surveying_Bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
