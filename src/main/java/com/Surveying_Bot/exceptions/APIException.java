526420a75d924fd9ddfc19802ad3502236376538
archisman <archiarchisman@gmail.com>
2025-05-21 02:09:14 +0530

    fix: custom exceptions

--- src/main/java/com/Surveying_Bot/exceptions/APIException.java
+++ src/main/java/com/Surveying_Bot/exceptions/APIException.java
@@ -1,6 +1,9 @@
 package com.Surveying_Bot.exceptions;
 
+import java.io.Serial;
+
 public class APIException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public APIException(String message, Throwable cause){
--- src/main/java/com/Surveying_Bot/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/Surveying_Bot/exceptions/ResourceNotFoundException.java
@@ -1,8 +1,10 @@
 package com.Surveying_Bot.exceptions;
 
+import java.io.Serial;
 import java.util.UUID;
 
 public class ResourceNotFoundException extends RuntimeException{
+    @Serial
     private static final long serialVersionUID= 1L;
 
     String resourceName;
@@ -12,14 +14,14 @@ public class ResourceNotFoundException extends RuntimeException{
     UUID fieldUUID;
 
     public ResourceNotFoundException(String resourceName, String field, String fieldName){
-        super(String.format("%s is not found with %s: %s"));
+        super(String.format("%s is not found with %s: %s", resourceName, field, fieldName));
         this.resourceName = resourceName;
         this.field = field;
         this.fieldName = fieldName;
     }
 
     public ResourceNotFoundException(String resourceName, String field, Long fieldId){
-        super(String.format("%s is not found with %s: %s"));
+        super(String.format("%s is not found with %s: %s", resourceName, field, fieldId));
         this.resourceName = resourceName;
         this.field = field;
         this.fieldId = fieldId;
